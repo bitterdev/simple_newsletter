f88c168adab563f76c005486fb675ae0dec83746
Fabian Bitter <fabianbitter@MacBook-Air-von-Fabian-2.local>
2023-08-22 23:39:45 +0200

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,6 @@ module.exports = function (grunt) {
                     {src: ['blocks/**'], dest: "dist/"},
                     {src: ['controllers/**'], dest: "dist/"},
                     {src: ['elements/**'], dest: "dist/"},
-                    {src: ['jobs/**'], dest: "dist/"},
                     {src: ['js/**'], dest: "dist/"},
                     {src: ['languages/**'], dest: "dist/"},
                     {src: ['mail/**'], dest: "dist/"},
@@ -76,7 +75,7 @@ module.exports = function (grunt) {
         compress: {
             main: {
                 options: {
-                    archive: 'build/' + packageName + '.zip'
+                    archive: 'release/' + packageName + '.zip'
                 },
                 files: [
                     {src: ['**'], dest: packageName, expand: true, cwd: 'dist/'}
@@ -86,25 +85,14 @@ module.exports = function (grunt) {
         clean: {
             dist: ['dist'],
             composer: ['vendor', 'composer.lock']
-        },
-        phpcsfixer: {
-            app: {
-                dir: 'dist'
-            },
-            options: {
-                bin: './vendor/friendsofphp/php-cs-fixer/php-cs-fixer',
-                usingCache: "no",
-                quiet: true
-            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-composer');
-    grunt.loadNpmTasks('grunt-php-cs-fixer');
     grunt.loadNpmTasks('grunt-version');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['clean:dist', 'copy', 'version', 'clean:composer', 'composer:dev:install', 'phpcsfixer', 'compress:main', 'clean:dist']);
+    grunt.registerTask('default', ['clean:dist', 'copy', 'version', 'clean:composer', 'composer:dev:install', 'compress:main', 'clean:dist']);
 };
